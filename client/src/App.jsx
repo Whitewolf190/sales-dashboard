492f711addd1c2b0e8552c24f9054469abb1ed15
Whitewolf190 <mryashkale190@gmail.com>
2024-08-22 15:49:46 +0530

    "Changes 3"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import TotalSalesChart from './components/totalsales'
 import SalesGrowthChart from './components/salesgrowth'
 import NewCustomersChart from './components/newcustomers'
 import CombinedCustomerChart from './components/repeatedCustomer'
-import CustomerGeography from './components/customerGeography'
+
 import Navbar from './components/Navbar'
 import './App.css'
 
